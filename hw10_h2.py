21da6069dcfa4907d424d735213763212d80c665
Nils Olsson <nilso@enosis.net>
2019-04-14 23:36:02 -0700

    Update

--- hw10_h2.py
+++ hw10_h2.py
@@ -8,14 +8,14 @@ def terms(p, x):
         yield t[0][0], t[1]
 
 # If $sum_{n\ge 0}n^d z^n=\frac{Q_d(z)}{{(1-z)}^{d+1}}$
-# then $Q_d(z)={(1-z)}^{d+1}\cdot sum_{n\ge 0}n^d z^n$
+# => $Q_d(z)={(1-z)}^{d+1}\cdot sum_{n\ge 0}n^d z^n$
 #
 # @param z Value to evaluate $Q_d(z)$ at.
 # @param d Degree.
 # @return $Q_d(z)$ evaluated with parameters $z$ and $d$.
 def Q(z, d):
     a = (1-x)**(d+1)*sum(n**d*x**n for n in range(d+1))
-    b = sum(c*x**p for p, c in terms(a, x) if p<(d+1))
+    b = sum(c*x**p for p, c in terms(a, x) if p <= d)
     return b.subs(x, z)
 
 if __name__ == '__main__':
